695dd8809288a6642f912d8bcc363d31a974906e
saswati-kar <165430236+saswati-kar@users.noreply.github.com>
2024-05-23 22:47:11 -0500

    Learn Git 2

---
+++ src/test/java/variousConcepts/LearnGit2.java
@@ -0,0 +1,28 @@
+package variousConcepts;
+
+public class LearnGit2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void printMe1()
+	{
+		System.out.println("git test 1");
+		
+	}
+	public void printMe2()
+	{
+		System.out.println("git test 2");
+		
+	}
+	
+	public void printMe3()
+	{
+		System.out.println("git test 3");
+		
+	}
+	
+	
+}
